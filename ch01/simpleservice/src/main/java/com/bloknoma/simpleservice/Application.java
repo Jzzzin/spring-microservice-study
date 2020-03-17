cc78044a475a47e004a21383ab96097df7125041
Jzzin <jzzzin@naver.com>
2020-03-17 10:32:29 +0900

    ch02_licensing_service complete

--- ch01/simpleservice/src/main/java/com/bloknoma/simpleservice/Application.java
+++ ch01/simpleservice/src/main/java/com/bloknoma/simpleservice/Application.java
@@ -1,4 +1,4 @@
-package springmicrostudy.simpleservice;
+package com.bloknoma.simpleservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

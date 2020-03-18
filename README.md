9d574f49514af89c30f7f5c4249bc75e23b19d0c
Jzzin <jzzzin@naver.com>
2020-03-18 12:50:36 +0900

    ch03_confsvr_and_licensing_service complete

---
+++ README.md
@@ -0,0 +1,8 @@
+1. 윈도우 환경에서 실습 시 run.sh 스크립트의 line separator 가 달라서 도커에서 스크립트가 실행되지 않을 수 있다. 
+
+다음 사이트를 참조하면 해결하도록 하자
+
+- https://seokr.tistory.com/670
+- https://smallgiant.tistory.com/19
+
+2. 
---
+++ ch03/licensing-service/src/main/java/com/bloknoma/licenses/services/LicenseService.java
@@ -0,0 +1,43 @@
+package com.bloknoma.licenses.services;
+
+import com.bloknoma.licenses.config.ServiceConfig;
+import com.bloknoma.licenses.model.License;
+import com.bloknoma.licenses.repository.LicenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class LicenseService {
+
+    @Autowired
+    private LicenseRepository licenseRepository;
+
+    @Autowired
+    ServiceConfig config;
+
+    public License getLicense(String organizationId, String licenseId) {
+        License license = licenseRepository.findByOrOrganizationIdAndLicenseId(organizationId, licenseId);
+        return license.withComment(config.getExampleProperty());
+    }
+
+    public List<License> getLicensesByOrg(String organizationId) {
+        return licenseRepository.findByOrganizationId(organizationId);
+    }
+
+    public void saveLicense(License license) {
+        license.withId(UUID.randomUUID().toString());
+
+        licenseRepository.save(license);
+    }
+
+    public void updateLicense(License license) {
+        licenseRepository.save(license);
+    }
+
+    public void deleteLicense(License license) {
+        licenseRepository.delete(license);
+    }
+}
